40ca67c212df541c2e3382cdeda457f11206a3f3
Shawn Hooper <shawn@fivesense.ca>
2022-11-15 17:22:32 -0500

    Import Tweets in the order they were written (based on id)

--- twitter-archive-to-wp.php
+++ twitter-archive-to-wp.php
@@ -7,7 +7,7 @@
  * Author URI:      https://shawnhooper.ca/
  * Text Domain:     birdsite-archive
  * Domain Path:     /languages
- * Version:         1.1.0
+ * Version:         1.1.1
  *
  * @package         Birdsite_Archive
  */
