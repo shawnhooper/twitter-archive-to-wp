2dc5fb671edb12492605060c67a66272fe5a5006
Shawn Hooper <shawn@fivesense.ca>
2022-12-22 12:48:07 -0500

    Configured as a WP-CLI package

--- twitter-archive-to-wp.php
+++ twitter-archive-to-wp.php
@@ -7,7 +7,7 @@
  * Author URI:      https://shawnhooper.ca/
  * Text Domain:     birdsite-archive
  * Domain Path:     /languages
- * Version:         2.0.2
+ * Version:         2.0.3
  *
  * @package         Birdsite_Archive
  */
